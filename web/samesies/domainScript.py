37b065ea5f36d092b4d87a02099340abca22be04
adbabcock <austin.babcock@dsu.edu>
2025-10-15 14:09:53 -0500

    dif way of doing hosts

---
+++ web/samesies/domainScript.py
@@ -0,0 +1,10 @@
+#!/usr/bin/exec-suid -- /usr/bin/python3 -I
+import sys
+sys.path.append('/challenge')
+
+if __name__ == '__main__':
+    with open("/etc/hosts", "a+") as f:
+        text = f.read()
+        if "minnetonka" not in text:
+            f.write("127.0.0.1:5000\tminnetonkatriplets.ctf")
+        
