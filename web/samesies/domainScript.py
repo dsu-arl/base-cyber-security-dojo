689114e6333894e39a2331c837f65bb99b2f9491
adbabcock <austin.babcock@dsu.edu>
2025-10-20 13:28:19 -0500

    suid pls

--- web/samesies/domainScript.py
+++ web/samesies/domainScript.py
@@ -1,4 +1,4 @@
-#!/usr/bin/exec-suid -- /usr/bin/python3 -I
+#!/usr/bin/exec-suid -- /usr/bin/python3
 import sys
 sys.path.append('/challenge')
 
--- web/samesies/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# add custom hosts if they aren't already present
-grep -q "minnetonkatriplets.test" /etc/hosts || echo "127.0.0.1 minnetonkatriplets.test" >> /etc/hosts
-grep -q "attacker.test" /etc/hosts || echo "127.0.0.1 attacker.test" >> /etc/hosts
-
-# exec the container's main process (preserve signals)
-exec "$@"
